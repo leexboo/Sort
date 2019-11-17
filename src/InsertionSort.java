75c9b568dbc988d2ac3be572a7bfa1f1340b769a
leexboo <386669232@qq.com>
2019-11-17 21:27:34 +0800

    插入排序

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 
+/**
+ * 插入排序
+ */
 public class InsertionSort {
     public static void InsertionSort(int[] arr){
         for(int i = 0; i < arr.length - 1; i++){
@@ -17,6 +20,25 @@ public class InsertionSort {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    /**
+     * 先将要交换的位置的值暂存，然后依次往前对比，如果比这个元素大则向后移动
+     * 直到没有对比到比这个元素小的元素，那么就插入要交换的元素
+     */
+    public static void InsertSort2(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            int temp = arr[i];
+            int j = i;
+            for(; j > 0; j--){
+                if(temp < arr[j-1]){
+                    arr[j] = arr[j - 1];
+                }else{
+                    break;
+                }
+            }
+            arr[j] = temp;
+        }
     }
 
     public static void main(String[] args) {
